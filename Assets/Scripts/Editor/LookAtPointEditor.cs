62f23baa9fbbfbe0e164f1cfa4b680179ad690d0
RonaldJudin <ronaljud@gmail.com>
2023-11-29 14:23:46 +0200

    13. praktikum ja tugiplokkide klassi kaunistamine.

---
+++ Assets/Scripts/Editor/LookAtPointEditor.cs
@@ -0,0 +1,30 @@
+//C# Example (LookAtPointEditor.cs)
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LookAtPoint))]
+[CanEditMultipleObjects]
+public class LookAtPointEditor : Editor
+{
+    SerializedProperty lookAtPoint;
+
+    void OnEnable()
+    {
+        lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(lookAtPoint);
+        serializedObject.ApplyModifiedProperties();
+        if (lookAtPoint.vector3Value.y > (target as LookAtPoint).transform.position.y)
+        {
+            EditorGUILayout.LabelField("(Above this object)");
+        }
+        if (lookAtPoint.vector3Value.y < (target as LookAtPoint).transform.position.y)
+        {
+            EditorGUILayout.LabelField("(Below this object)");
+        }
+    }
+}
--- Assets/Scripts/SupportBlock.cs
+++ Assets/Scripts/SupportBlock.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class SupportBlock : MonoBehaviour
 {
+    [Delayed]
     public string id;
+    [Space]
 
+    [Range(0.5f, 2)]
+    [DelayedAttribute] // Delayed ei tööta hästi! See siin samuti.
+    [Tooltip("Additively modifies the tower's base range. For example, 1.2 increases range by 20% of the base.")]
     public float RangeModifier;
-    public float FirerateModifier;
-    public float DamageModifier;
-    public float BulletSpeedModifier;
 
+    [Range(0.5f, 2)]
+    [DelayedAttribute]
+    [Tooltip("Additively modifies the tower's base firerate. For example, 1.2 increases firerate by 20% of the base.")]
+    public float FirerateModifier;
+
+    [Range(0.5f, 2)]
+    [DelayedAttribute]
+    [Tooltip("Additively modifies the tower's base damage. For example, 1.2 increases damage by 20% of the base.")]
+    public float DamageModifier;
+
+    [Range(0.5f, 2)]
+    [DelayedAttribute]
+    [Tooltip("Additively modifies the tower's base bullet speed. For example, 1.2 increases bullet speed by 20% of the base.")]
+    public float BulletSpeedModifier;
+    [Space]
+    [Tooltip("Whether bullets move intelligently towards enemies.")]
     public bool Seeking; // Kas kuul p��ab vaenlasi targalt
+    [Tooltip("Piercing bullets are not destroyed after hitting an enemy.")]
     public bool Piercing; // Kas kuul saab vaenlasest l�bi minna
+    [Tooltip("Persistent bullets linger on the ground like mines.")]
     public bool Persistent; // Kas kuul j��b p�rast teekonna l�petamist alles, kui vaenlast ei taba
+    [Space]
+    [Range(0, 10)]
+    [DelayedAttribute]
     public float Poison; // Vaenlastele on vaja atribuuti, mis iga tiksu j�rel neid kahjustab
+
+    [Range(-1, 1)]
+    [Delayed]
+    [Tooltip("Modifies hit enemies' moving speed. Negative values cause confusion and fear effects.")]
     public float Slow; // Vaenlaste liikumiskiiruse muutmine; negatiivne arv m�jub hirmuefektina
 }
